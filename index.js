b6c4c4b703a4c345af77433079219f065a8e5248
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-10-14 14:29:04 -0300

    API Tasks

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ const app = express();
 
 app.use('/',express.static('public'));
 
+app.get('/api/tasks', (req, res) => {
+    const lista = [
+        {id:1, descripcion: 'Pasear el Perro', fecha: '14-10-2025'},
+        {id:2, descripcion: 'Estudiar NodeJS', fecha: '18-10-2025'},
+        {id:3, descripcion: 'Ir al Cine', fecha: '18-10-2025'}
+    ];
+
+    res.status(200).json({msg:'ok', data: lista});
+})
+
+
 
 app.listen( PORT, () => console.log(`Servidor en el puerto ${PORT}`))
